3d2a140ef35f38857ce86c1abb3f2b4c5676f442
Francisco Briceño <fjavier.cgam@gmail.com>
2019-10-10 23:42:35 -0600

    Mostrar nombre por defecto
    Se agrega el nombre por defecto si no trae el parametro

--- src/main/java/ni/org/jug/servlet/GreetingServlet.java
+++ src/main/java/ni/org/jug/servlet/GreetingServlet.java
@@ -1,7 +1,10 @@
 package ni.org.jug.servlet;
 
 import java.io.IOException;
+
+import javax.naming.Context;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class GreetingServlet
  */
-@WebServlet("/saludo")
+@WebServlet(urlPatterns = "/saludo", 
+			name = "GreetingServlet", 
+			initParams = {@WebInitParam(name="nombre-defecto", value = "Mr. Duke")})
 public class GreetingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +32,7 @@ public class GreetingServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String nombre = request.getParameter("nombre");
+		String nombre = request.getParameter("nombre") == null ? getInitParameter("nombre-defecto") : request.getParameter("nombre");
 		response.getWriter().append("<h1>Bienvenido "+nombre+" </h1>");
 		response.getWriter().append("Mi primer Servlet desde :").append(request.getContextPath());
 	}
